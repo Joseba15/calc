f713f0ff72c20cec6e869d8c899b47c7a01f5d97
Joseba15 <josebadg15@gmail.com>
2022-03-22 08:54:38 +0100

    Subido el src del proyecto calc

---
+++ src/com/calc/model/Aritmetica.java
@@ -0,0 +1,65 @@
+package com.calc.model;
+
+public class Aritmetica {
+
+	public Aritmetica() {}
+
+	
+	/**
+	 * Este método permite sumar dos numeros, que segun el tipo de numero, la suma de los numeros es distinta 
+	 * @param n1 primer numero que vamos a sumar, puede ser de tipo decimal o bien entero
+	 * @param n2 segundo numero que vamos a sumar, puede ser de tipo decimal o bien entero
+	 * @return resultado, guarda la suma de los numeros que se introducen por parámetro 
+	 */
+	
+	public Numero suma(Numero n1, Numero n2) {
+		Numero resultado = null;
+		
+		if(n1.getBase().equals(n2.getBase())) {
+			
+			if (n1 instanceof Entero && n2 instanceof Entero) {
+				resultado = new Entero(n1.getValorEntero()+n2.getValorEntero());
+				
+			}else if(n1 instanceof Decimal && n2 instanceof Decimal) {
+				resultado = new Decimal(n1.getValorEntero()+n2.getValorEntero(),
+						((Decimal)n1).getValorDecimal()+((Decimal)n2).getValorDecimal(),
+						n1.getBase()
+				);
+			}else if(n1 instanceof Decimal) {
+				resultado = new Decimal(n1.getValorEntero()+n2.getValorEntero(),
+						((Decimal)n1).getValorDecimal(),
+						n1.getBase());
+			}else {
+				resultado = new Decimal(n1.getValorEntero()+n2.getValorEntero(),
+						((Decimal)n2).getValorDecimal(),
+						n1.getBase());
+			}
+			
+		}
+		
+		return resultado;
+	}
+	
+	public Numero resta(Numero n1, Numero n2) {
+		Numero sustraer = null;
+		if (n2 instanceof Decimal) {
+			Decimal tmp = (Decimal)n2;
+			sustraer = new Decimal(-tmp.getValorEntero(), -tmp.getValorDecimal(), tmp.getBase()); 
+		}else {
+			sustraer = new Entero(-n2.getValorEntero());
+		}
+		return suma(n1, sustraer);
+	}
+	
+	
+	
+	public Numero calcularMedia(Object[] objs) {
+		Numero acumulador = new Entero(0);
+		
+		for(int i=0; i<objs.length; i++) {
+			acumulador = suma(acumulador, (Numero)objs[i]);
+		}
+		return new Decimal(acumulador.getValorEntero()/objs.length, 
+						((Decimal)acumulador).getValorDecimal()*objs.length);
+	}
+}
